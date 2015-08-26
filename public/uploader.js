b92b351830a2cab437363f46c5cf44f07f85420d
fono09 <fono@fono.jp>
2015-08-26 14:12:02 +0900

    Update WebUI

--- public/uploader.js
+++ public/uploader.js
@@ -11,15 +11,21 @@ $(function(){
 		console.log(form_data);
 
 		$.ajax({
-			url: 'http://fono.jp:4567/upload',
+			url: 'http://uploader.fono.jp/upload',
+			async: true,
+			xhr: function(){
+				__xhr__ = $.ajaxSettings.xhr();
+				__xhr__.upload.addEventListener("progress", updateProgressbar, false);
+				return __xhr__;
+			},
 			method: 'POST',
 			dataType: 'json',
 			contentType: false,
 			processData: false,
 			data: form_data,
 		}).done(function(result){
-			console.log('success',res);
-		}).fail(function( jqXHR, textStatus, errorThrown ){
+			console.log('success',result);
+		}).fail(function(jqXHR, textStatus, errorThrown){
 			console.log('fail',jqXHR,textStatus,errorThrown);
 		});
 
@@ -27,6 +33,32 @@ $(function(){
 	});
 
 });
+
+$(document).ready(function(){
+	
+	$.ajax({
+		url: 'http://uploader.fono.jp/list',
+		async: true,
+		method: 'GET',
+		dataType: 'json',
+	}).done(function(result){
+		console.log('success',result);
+	}).fail(function(jqXHR, textStatus, errorThrown){
+		console.log('fail',jqXHR,textStatus,errorThrown);
+	});
+
+
+});
+	
+
+function updateProgressbar(e){
+
+	console.log((e.loaded / e.total)*100 + "%");
+	$('#upload_progress').val(Math.floor(e.loaded / e.total * 100));
+	
+	return e;
+
+}
 
 
 
