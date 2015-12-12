808a382ee8cd3f93f450a5e070a608cff2b615e8
fono09 <fono@fono.jp>
2015-12-13 00:11:43 +0900

    Relace button group with pagination

--- public/uploader.js
+++ public/uploader.js
@@ -13,7 +13,7 @@ $(function(){
 		var token_id = 'up'+genUID();
 		var token_name = $('[name=body]').prop('files')[0].name;
 		$(document).trigger('createProgressbar',token_id);
-		
+
 		$.ajax({
 			url: 'https://uploader.fono.jp/upload',
 			async: true,
@@ -53,16 +53,18 @@ $(document).ready(function(){
 		$(this).trigger('drawTable');
 	}	
 
-	if($("#pager").length){
-		$(this).trigger('drawPager');
-	}
-
 	if($(".dl_locked").length){
 		$(this).trigger('setLockedLink')
 	}
 
+}).on('drawTable', function(d,num){
+	console.log('drawTable');
 
-}).on('drawPager', function(){
+	if(!num){
+		num = 1;
+	}
+	var pager = $('.switcher .pagination');
+
 	$.ajax({
 		url: 'https://uploader.fono.jp/info',
 		async: true,
@@ -70,81 +72,113 @@ $(document).ready(function(){
 		dataType: 'json',
 	}).done(function(result){
 		
-		for(var i=0; i < result.pages; i++){
-			$('<button>').attr('onclick','$(document).trigger(\'drawTable\','+(i+1)+')').addClass('btn btn-default').attr('type','button').text(i+1).appendTo($('#pager'));
-		}
-		$('<button>').on('click',function(){$(document).trigger('drawTable','all')}).addClass('btn btn-default').attr('type','button').text('all').appendTo($('#pager'));
-	});
-
-}).on('drawTable', function(d,num){
-	console.log('drawTable');
-	
-	var url = 'https://uploader.fono.jp/list'
-	if(typeof num == 'number'){
-		url += '/'+num;
-	}else if(!num){
-		url += '/1';
-	}
-
-	$.ajax({
-		url: url,
-		async: true,
-		method: 'GET',
-		dataType: 'json',
-	}).done(function(result){
-		console.log('success',result);
-		var table = $("#file_list");
-		table.empty();
-
-		result.forEach(function(row){
-			var tr = $('<tr>').appendTo(table);
-
-			var dl_link = $('<a>').attr('href','/download/' + row.id);
-			if(row.dl_locked){
-				dl_link.addClass('dl_locked').on('click',function(e){ e.preventDefault(); ajaxPostDownload(row.id) });
-			}
-			dl_link.text(row.name);
-
-			var del_link = $('<a>').attr('href','#').on('click',function(e){ e.preventDefault(); ajaxPostDelete(row.id) });
-			del_link.text('locked');
+		var prev_idx = $('.switcher .pagination li').index($('.switcher .pagination li.active'));
+		//生成前prev_idx = -1,再生成時は値の保全となる為必ず再生成前に実行
+		
+		if($('.switcher .pagination li').length < result.pages+2){
+			$('.switcher .pagination').empty();
 			
-			var tw_dl_url = encodeURI('https://uploader.fono.jp/cushon/'+row.id);
-
-			var tw_link = $('<a>').attr('href',
-				'https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fuploader.fono.jp%2F&ref_src=twsrc%5Etfw&text=Download%20'
-				+ row.name + ' ' + tw_dl_url
-				).text('Tweet').on('click', function(){
-					window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1');
-					return false;
-				});
-
-			$('<td>').text(row.id).appendTo(tr);
-			$('<td>').append(dl_link).appendTo(tr);
-			$('<td>').text(row.comment).appendTo(tr);
-			$('<td>').text(row.last_updated).appendTo(tr);
-
-			if(row.del_locked){
-				$('<td>').append(del_link).appendTo(tr);
-			}else{
-				$('<td>').text('free').appendTo(tr);
+			var current = $('<li>').attr('onclick','$(document).trigger(\'drawTable\',\'prev\')').appendTo(pager);
+			$('<a>').attr('href','#').attr('aria-label','Previous').html('<span aria-hidden="true">&laquo;</span>').appendTo(current);
+				
+			for(var i = 0; i < result.pages; i++){
+				
+				current = $('<li>').attr('onclick','$(document).trigger("drawTable",'+(i+1)+')').appendTo(pager);
+				$('<a>').attr('href','#').text(i+1).appendTo(current);
 			}
+			
+			current = $('<li>').attr('onclick','$(document).trigger("drawTable","next")').appendTo(pager);
+			$('<a>').attr('href','#').attr('aria-label','Next').html('<span aria-hidden="true">&raquo;</span>').appendTo(current);
 
-			$('<td>').text(row.dl_locked?'locked':'free').appendTo(tr);
-			$('<td>').append(tw_link).appendTo(tr);
+		}
+		//足りないか存在しなければ(再)生成
+		
+		if(num == 'prev'){
+			num = prev_idx - 1;
+			if(prev_idx == 1){
+				num = result.pages;
+			}
+		}else if(num=='next'){
+			num = prev_idx + 1;
+			if(prev_idx == result.pages){
+				num = 1;
+			}
+		}else if(typeof num == "string"){
+			num = 1;
+		}
+		var table_data_url = 'https://uploader.fono.jp/list/'+num;
+		//相対のポジション指定
+
+
+		if(prev_idx == -1){
+			$('.switcher .pagination li').eq(1).addClass('active');
+		}else if(prev_idx != num){
+			$('.switcher .pagination li.active').removeClass('active');
+			$('.switcher .pagination li').eq(num).addClass('active');
+		}
+		//以前のポジション取得して違えばポジション更新
+
+		$.ajax({
+			url: table_data_url,
+			async: true,
+			method: 'GET',
+			dataType: 'json',
+		}).done(function(result){
+			console.log('success',result);
+			var table = $("#file_list");
+			table.empty();
+
+			result.forEach(function(row){
+				var tr = $('<tr>').appendTo(table);
+
+				var dl_link = $('<a>').attr('href','/download/' + row.id);
+				if(row.dl_locked){
+					dl_link.addClass('dl_locked').on('click',function(e){ e.preventDefault(); ajaxPostDownload(row.id) });
+				}
+				dl_link.text(row.name);
+
+				var del_link = $('<a>').attr('href','#').on('click',function(e){ e.preventDefault(); ajaxPostDelete(row.id) });
+				del_link.text('locked');
+
+				var tw_dl_url = encodeURI('https://uploader.fono.jp/cushon/'+row.id);
+
+				var tw_link = $('<a>').attr('href',
+					'https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fuploader.fono.jp%2F&ref_src=twsrc%5Etfw&text=Download%20'
+					+ row.name + ' ' + tw_dl_url
+					).text('Tweet').on('click', function(){
+						window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1');
+						return false;
+					});
+
+				$('<td>').text(row.id).appendTo(tr);
+				$('<td>').append(dl_link).appendTo(tr);
+				$('<td>').text(row.comment).appendTo(tr);
+				$('<td>').text(row.last_updated).appendTo(tr);
+
+				if(row.del_locked){
+					$('<td>').append(del_link).appendTo(tr);
+				}else{
+					$('<td>').text('free').appendTo(tr);
+				}
+
+				$('<td>').text(row.dl_locked?'locked':'free').appendTo(tr);
+				$('<td>').append(tw_link).appendTo(tr);
+			});
+
+
+			var label = $('<tr>').prependTo(table);
+			$('<td>').text('ID').appendTo(label);
+			$('<td>').text('NAME').appendTo(label);
+			$('<td>').text('COMMENT').appendTo(label);
+			$('<td>').text('DATE').appendTo(label);
+			$('<td>').text('DEL').appendTo(label);
+			$('<td>').text('DL').appendTo(label);
+			$('<td>').text('TWEET').appendTo(label);
+
+		}).fail(function(jqXHR, textStatus, errorThrown){
+			console.log('fail',jqXHR,textStatus,errorThrown);
 		});
 
-		
-		var label = $('<tr>').prependTo(table);
-		$('<td>').text('ID').appendTo(label);
-		$('<td>').text('NAME').appendTo(label);
-		$('<td>').text('COMMENT').appendTo(label);
-		$('<td>').text('DATE').appendTo(label);
-		$('<td>').text('DEL').appendTo(label);
-		$('<td>').text('DL').appendTo(label);
-		$('<td>').text('TWEET').appendTo(label);
-
-	}).fail(function(jqXHR, textStatus, errorThrown){
-		console.log('fail',jqXHR,textStatus,errorThrown);
 	});
 
 
@@ -155,7 +189,7 @@ $(document).ready(function(){
 	var inner = $('<div>').addClass('progress-bar progress-bar-striped active').attr('role','progressbar').attr('aria-valuenow',0).attr('aria-valuemin',0).attr('aria-valuemax',100);
 
 	inner.appendTo(outer);
-	
+
 }).on('updateProgressbar', function(d,e,token_id,token_name){
 	if(e == null){return};
 
@@ -171,51 +205,51 @@ $(document).ready(function(){
 	$('.progress_list [token='+token_id+']').remove();
 
 }).on('setLockedLink', function(d,e){
-	
+
 	$('.dl_locked').on('click',function(e){ e.preventDefault(); e.currentTarget.href.match(/\/(\d+)$/); ajaxPostDownload(RegExp.$1) });
 
 });
 
 function genUID(){
 	var rand = Math.floor(Math.random()*1000)
-	var date = new Date();
+		var date = new Date();
 	var time = date.getTime();
 	return rand + time.toString();
 }
-	
+
 function ajaxPostDownload(id){
 	console.log('ajaxPostDownload('+id+')');
-	
-	var dlpass = window.prompt('Download Password Required','');
-	$.ajax({
-		url: 'https://uploader.fono.jp/download/'+id,
-		method: 'POST',
-		dataType: 'json',
-		data: 'dlpass='+dlpass,
-	}).done(function(result){
-		console.log('sessionWriteSuccess',result);
-		location.href='https://uploader.fono.jp/download/'+id;
-	}).fail(function(result){
-		console.log('sessionWriteFail',result);
-		alert('Authentication Failed');
-	});
-}
 
-function ajaxPostDelete(id){
-	console.log("ajaxPostDelete("+id+")");
+			var dlpass = window.prompt('Download Password Required','');
+			$.ajax({
+				url: 'https://uploader.fono.jp/download/'+id,
+				method: 'POST',
+				dataType: 'json',
+				data: 'dlpass='+dlpass,
+			}).done(function(result){
+				console.log('sessionWriteSuccess',result);
+				location.href='https://uploader.fono.jp/download/'+id;
+			}).fail(function(result){
+				console.log('sessionWriteFail',result);
+				alert('Authentication Failed');
+			});
+			}
 
-	var delpass = window.prompt('Delete Password Required','');
-	$.ajax({
-		url: 'https://uploader.fono.jp/delete/'+id,
-		method: 'POST',
-		detaType: 'json',
-		data: 'delpass='+delpass,
-	}).done(function(result){
-		console.log('deleteSuccess',result);
-		$(document).trigger('drawTable');
-		alert('Delete Succeeded');
-	}).fail(function(result){
-		console.log('deleteFailed',result);
-		alert('Authentication Failed');
-	});
-}
+			function ajaxPostDelete(id){
+				console.log("ajaxPostDelete("+id+")");
+
+				var delpass = window.prompt('Delete Password Required','');
+				$.ajax({
+					url: 'https://uploader.fono.jp/delete/'+id,
+					method: 'POST',
+					detaType: 'json',
+					data: 'delpass='+delpass,
+				}).done(function(result){
+					console.log('deleteSuccess',result);
+					$(document).trigger('drawTable');
+					alert('Delete Succeeded');
+				}).fail(function(result){
+					console.log('deleteFailed',result);
+					alert('Authentication Failed');
+				});
+			}
