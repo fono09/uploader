f0d79556d2ab76e21b3ae0975b94d8374e7c9df6
fono09 <fono@fono.jp>
2017-05-04 12:33:10 +0900

    Remove useless pid, Repair tweet link, Use docker-compose without systemd

--- unicorn.rb
+++ unicorn.rb
@@ -6,7 +6,5 @@ working_directory @dir
 timeout 300
 listen "#{@dir}run/unicorn.sock"
 
-pid "#{@dir}run/unicorn.pid"
-
 stderr_path "#{@dir}log/unicorn.stderr.log"
 stdout_path "#{@dir}log/unicorn.stdout.log"
