9cdd9c26d9d06dfbdb1bfb0f14f9922f2b7a988d
fono09 <fono@fono.jp>
2017-05-13 08:59:40 +0900

    Edit timezone settings

--- uploader.rb
+++ uploader.rb
@@ -13,6 +13,7 @@ set :session_secret, SecureRandom.hex(100)
 set :views, settings.root + '/templates'
 set :erb, :escape_html => true
 
+Time.zone = "Tokyo"
 ActiveRecord::Base.default_timezone = :local
 
 ActiveRecord::Base.establish_connection(
